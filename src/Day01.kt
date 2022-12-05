c68396702d950c0ddf98f7c6efd16ebfee3929e2
Valeriy Andrikeev <v.andrikeev@gmail.com>
2022-12-05 13:37:35 +0300

    Day #1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,53 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var maxCals = 0
+        var curCal = 0
+        input.forEachIndexed { index, line ->
+            if (line.isNotBlank()) {
+                curCal += line.toInt()
+            }
+            if (line.isBlank() || index == line.lastIndex) {
+                if (curCal > maxCals) {
+                    maxCals = curCal
+                }
+                curCal = 0
+            }
+        }
+        return maxCals
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val topList = intArrayOf(0, 0, 0)
+        var curCal = 0
+        input.forEachIndexed { index, line ->
+            if (line.isNotBlank()) {
+                curCal += line.toInt()
+            }
+            if (line.isBlank() || index == input.lastIndex) {
+                when {
+                    curCal > topList[2] -> {
+                        topList[0] = topList[1]
+                        topList[1] = topList[2]
+                        topList[2] = curCal
+                    }
+                    curCal > topList[1] -> {
+                        topList[0] = topList[1]
+                        topList[1] = curCal
+                    }
+                    curCal > topList[0] -> {
+                        topList[0] = curCal
+                    }
+                }
+                curCal = 0
+            }
+        }
+        return topList.sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
 
     val input = readInput("Day01")
     println(part1(input))
